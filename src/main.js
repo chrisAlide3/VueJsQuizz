5931137d4756a2430de1e2da17b4b4a36c4cc3ca
krispea3 <christian.bracher@gmail.com>
2019-04-27 09:59:10 +0800

    Deleted old versin of the app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import VueResource from "vue-resource";
+import VueRouter from "vue-router";
 import App from './App.vue'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+
 
 Vue.http.options.root = 'https://vue-http-add0e.firebaseio.com/data.json';
 
